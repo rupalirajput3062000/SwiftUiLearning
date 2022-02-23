0fe606058cbb7ab2f5c83feda3c3d227f6e49b41
Rupali Rajput <rurajput@expediagroup.com>
2022-02-23 17:10:35 +0530

    Initial Commit

---
+++ FirstSwiftUiProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUiProject
+//
+//  Created by Rupali Rajput on 23/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstSwiftUiProject/FirstSwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUiProjectApp.swift
+//  FirstSwiftUiProject
+//
+//  Created by Rupali Rajput on 23/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
